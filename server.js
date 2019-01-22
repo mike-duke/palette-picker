f66aeba4b25ecfa1b42745cb3f010b55e6ba774d
Mike Duke <aithon@gmail.com>
2019-01-22 16:46:23 -0700

    Add index.html file. Setup index.html as static asset

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const app = express();
 app.set('port', process.env.POST || 3000);
 app.locals.title = 'Palette Picker';
 
+app.use(express.static('public'));
+
 app.get('/', (request, response) => {
-  response.send('Oh Hello');
+  
 });
 
 app.listen(app.get('port'), () => {
-  console.log(app.locals.title + 'is running on port ' + app.get('port'));
+  console.log(app.locals.title + ' is running on port ' + app.get('port'));
 });
