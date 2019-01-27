ed1a73d81ae6c8f4340d6f3c2e962215eb8e5fc3
Mike Duke <aithon@gmail.com>
2019-01-27 13:58:41 -0700

    New palette saves correctly to a project and updates on the page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -93,24 +93,34 @@ const saveProject = (event) => {
 
 const savePalette = (event) => {
   event.preventDefault();
-  const paletteName = document.querySelector('.new-palette-input').value;
+  const name = document.querySelector('.new-palette-input').value;
   const colors = [];
   document.querySelectorAll('.hexcode').forEach((element) => {
     colors.push(element.innerText);
   });
-  const projectName = document.querySelector('#project-select').value;
+  const project_id = document.querySelector('#project-select').value;
+
   fetch('/api/v1/palettes', {
     method: 'POST',
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      name: paletteName,
-      colors,
-      projectName
+      name,
+      color1: colors[0],
+      color2: colors[1],
+      color3: colors[2],
+      color4: colors[3],
+      color5: colors[4],
+      project_id
     })
   })
-    .then(response => console.log(response))
+    .then(response => {
+      console.log(response);
+      const cards = document.querySelectorAll('.project-card');
+      cards.forEach(card => card.remove());
+      fetchProjects();
+    })
 }
 
 const populateOptions = () => {
@@ -121,10 +131,10 @@ const populateOptions = () => {
       const option = document.createElement('option');
       option.innerText = 'Select a project';
       select.append(option);
+
       results.projects.forEach((project) => {
-      const option = document.createElement('option');
-        option.setAttribute('value', project.name);
-        option.setAttribute('data-id', project.id);
+        const option = document.createElement('option');
+        option.setAttribute('value', project.id);
         option.innerText = project.name;
         select.append(option);    
       })
